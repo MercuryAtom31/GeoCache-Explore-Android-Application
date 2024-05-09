25d1803d0dc0d2e8771cd6c236865807f0bd54d4
MercuryAtom31 <83618354+MercuryAtom31@users.noreply.github.com>
2024-05-09 16:47:51 -0400

    Fixed the issue with the RecyclerView not displaying the list of geocaches as expected.

--- app/src/main/java/com/example/geocacheexploreandroidapplication/CacheListFragment.kt
+++ app/src/main/java/com/example/geocacheexploreandroidapplication/CacheListFragment.kt
@@ -1,30 +1,68 @@
 package com.example.geocacheexploreandroidapplication
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class CacheListFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
     private val cacheList = mutableListOf<Geocache>()
     private lateinit var adapter: CacheListAdapter
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cache_list, container, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        recyclerView = view.findViewById(R.id.recyclerView) // Make sure this ID matches your RecyclerView ID in XML
+        recyclerView.layoutManager = LinearLayoutManager(context)
         adapter = CacheListAdapter(cacheList)
-//        view.findViewById<RecyclerView>(R.id.cache_list_recyclerview).apply {
-//            layoutManager = LinearLayoutManager(context)
-//            adapter = this@CacheListFragment.adapter
-//        }
-        view.findViewById<RecyclerView>(R.id.recyclerView).apply {
-            layoutManager = LinearLayoutManager(context)
-            adapter = this@CacheListFragment.adapter
-        }
+        recyclerView.adapter = adapter
     }
 
     // Method to update the list and refresh the RecyclerView
     fun addCacheToList(cache: Geocache) {
-        adapter.addCache(cache)
+        cacheList.add(cache)  // Update your data source
+        adapter.notifyItemInserted(cacheList.size - 1)  // Notify the adapter of item inserted
     }
 }
+
+
+
+
+//package com.example.geocacheexploreandroidapplication
+//
+//import android.os.Bundle
+//import android.view.View
+//import androidx.fragment.app.Fragment
+//import androidx.recyclerview.widget.LinearLayoutManager
+//import androidx.recyclerview.widget.RecyclerView
+//
+//class CacheListFragment : Fragment() {
+//    private val cacheList = mutableListOf<Geocache>()
+//    private lateinit var adapter: CacheListAdapter
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        adapter = CacheListAdapter(cacheList)
+////        view.findViewById<RecyclerView>(R.id.cache_list_recyclerview).apply {
+////            layoutManager = LinearLayoutManager(context)
+////            adapter = this@CacheListFragment.adapter
+////        }
+//        view.findViewById<RecyclerView>(R.id.recyclerView).apply {
+//            layoutManager = LinearLayoutManager(context)
+//            adapter = this@CacheListFragment.adapter
+//        }
+//    }
+//
+//    // Method to update the list and refresh the RecyclerView
+//    fun addCacheToList(cache: Geocache) {
+//        adapter.addCache(cache)
+//    }
+//}
--- app/src/main/java/com/example/geocacheexploreandroidapplication/MainActivity.kt
+++ app/src/main/java/com/example/geocacheexploreandroidapplication/MainActivity.kt
@@ -23,6 +23,17 @@ class MainActivity : AppCompatActivity(), AddCacheDialogFragment.AddCacheDialogL
         }
     }
 
+//    override fun onDialogPositiveClick(cache: Geocache) {
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
+//        val cacheListFragment = navHostFragment?.childFragmentManager?.fragments?.find { it is CacheListFragment } as? CacheListFragment
+//        if (cacheListFragment != null) {
+//            cacheListFragment.addCacheToList(cache)
+//            Toast.makeText(this, "Cache '${cache.name}' added!", Toast.LENGTH_LONG).show()
+//        } else {
+//            Toast.makeText(this, "Error: Cache list fragment not found", Toast.LENGTH_LONG).show()
+//        }
+//    }
+
 
     // This function will be called when the "Cancel" button of the dialog is clicked
     override fun onDialogNegativeClick() {
