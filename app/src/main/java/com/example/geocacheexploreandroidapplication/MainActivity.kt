2a6f9fd5003bdc0148f98a1df85c6dce00a4c71a
MercuryAtom31 <83618354+MercuryAtom31@users.noreply.github.com>
2024-05-16 16:39:27 -0400

    DONE!!! Everything is perfectly working. Geocache Explore is fully functional ✅

--- app/src/main/java/com/example/geocacheexploreandroidapplication/MainActivity.kt
+++ app/src/main/java/com/example/geocacheexploreandroidapplication/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.example.geocacheexploreandroidapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity(), AddCacheDialogFragment.AddCacheDialogListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        // Dark Mode
+//        val isDarkModeEnabled = true
+//        if (isDarkModeEnabled) {
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+//        } else {
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+//        }
     }
     // This function will be called when the "OK" button of the dialog is clicked
     override fun onDialogPositiveClick(cache: Geocache) {
@@ -22,7 +34,7 @@ class MainActivity : AppCompatActivity(), AddCacheDialogFragment.AddCacheDialogL
             Toast.makeText(this, "Error: Cache list fragment not found", Toast.LENGTH_LONG).show()
         }
     }
-    // This function will be called when the "Cancel" button of the dialog is clicked
+//     This function will be called when the "Cancel" button of the dialog is clicked
     override fun onDialogNegativeClick() {
         Toast.makeText(this, "Add cache cancelled.", Toast.LENGTH_SHORT).show()
     }
